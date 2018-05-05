2b39a9aebcd5281243487c846fdba8673a36b0a9
Md Shafiqul Islam <uzzal2k5@gmail.com>
2018-05-05 13:06:03 +0600

    Create polisContainer.sh
    This file have to run from terminal to build and deploy polis container

---
+++ polisContainer.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+OSNAME=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
+if [ ${OSNAME}=="CentOS Linux" ];
+then
+    sh docker-centos.sh
+elif [ ${OSNAME}=="Ubuntu" ];
+then
+    sh docekr-ubuntus.h
+ fi
+
+
+mkdir POLIS_CONTAINER && cd POLIS_CONTAINER
+git clone -b master https://github.com/uzzal2k5/polispostgres.git
+git clone -b master https://github.com/uzzal2k5/polisserver.git
+git clone -b master https://github.com/uzzal2k5/polis_adminparticipants.git
+giot clone -b master https://github.com/uzzal2k5/polismath.git
+
+docker-compose -f polisDocker-compose.yml up
