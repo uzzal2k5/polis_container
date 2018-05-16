8779e8059de399b8167ff274851b67e51d7c8da6
uzzal2k5 <uzzal2k5@gmail.com>
2018-05-16 14:02:33 +0600

    for loop array update

--- polis-build.sh
+++ polis-build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-repo=('polisserver','polisClientAdmin','polisClientParticipation','polismath')
+repo=(polisserver polisClientAdmin polisClientParticipation polismath)
 
-for i in "${repo[@]}" ;
+for i in ${repo[@]} ;
 do
 cd ..
-git clone https://github.com/uzzal2k5/$i.git
+git clone https://github.com/uzzal2k5/${repo[i]}.git
 done
 cd ..
 cp polis_container/docker-compose-build.yml .
